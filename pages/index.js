494478ff23b2992a92e424e700c04bb31f94e933
Gus Coelho <gustavocoelho.adm@gmail.com>
2020-05-22 02:13:04 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -4,14 +4,6 @@ import Navbarside from "../components/Navbarside";
 function Index() {
   return (
     <div className="container">
-      <h1>SuperSquash.com.br</h1>
-      <p>Teste menu lateral</p>
-      <p>
-        Fabinho, veja a divisão de assuntos que fiz, segundo tua última
-        listagem, eu mudei o título de alguns e reescrevi alguns dos sub-itens,
-        me diga o que acha. Se tu clicar no menu abaixo, ele vai mostrar os
-        sub-itens.
-      </p>
       <Navbarside />
     </div>
   );
